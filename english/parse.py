6dc6496bebcdeb4c758c3382e1f1b19a3e395837
David A Roberts <d@vidr.cc>
2020-12-05 12:32:17 +1000

    more accurate parsing

--- english/parse.py
+++ english/parse.py
@@ -10,25 +10,25 @@ class Sem(lark.Transformer):
   def disj(self, *args):
     return ' | '.join(args)
   def join(self, rel, *args):
+    if len(args) == 0: return rel
     if len(args) == 1 and ' ' not in args[0]:
       return f"{rel}.{args[0]}"
     return f"{rel}[{','.join(args)}]"
   def flip(self, verb):
     return f"{verb}'"
-  def neg(self, adj):
-    return f"~{adj}"
+  def neg(self, x):
+    return f"~{x}"
   def flipjoin(self, noun, verb, *args):
     return self.join(self.flip(verb), noun, *args)
-  def flipjoin2(self, verb, noun, *args):
+  def flipjoin2(self, verb, noun=None, *args):
+    if not noun: return verb
     return self.join(self.flip(verb), noun, *args)
   def joint(self, rel, noun, time=None):
     if not time: return self.join(rel, noun)
     return f"{rel}[{noun}; {time}]"
+  def rjoin(self, n, v):
+    return self.join(v, n)
 
-  def _ambig(self, args):
-    args = set(args)
-    if len(args) > 1: return args
-    return args.pop()
   def command(self, verb, noun):
     return f"{verb}({noun})!"
   def put(self, noun, prep, location):
@@ -40,95 +40,50 @@ class Sem(lark.Transformer):
     return f"{noun}!"
   def question(self, *args):
     return self.conj(*args) + '?'
-  def passive(self, noun, verb):
-    return self.join(verb, noun)
   def count(self, *args):
     return f"#count({self.conj(*args)})"
-  def count2(self, noun, verb, time):
-    return f"#count({self.conj(self.join(verb, noun), time)})"
-  def qwhen(self, verb, noun, time):
-    return self.conj(self.join(verb, noun), time)
   def statement(self, noun, adj):
     return f"{adj}({noun})."
-  def but(self, v1, n1, v2, n2):
-    if v1 == v2:
-      return v1 + '(' + self.conj(n1, self.neg(n2)) + ')'
-  def define(self, noun, adj):
-    return f"{noun}({adj})."
   def verb(self, name):
     return name.replace(' ','')
-  def noun(self, *args):
+  def verbp(self, v, p=''):
+    return v + p
+  def np(self, *args):
     return self.conj(*args)
-  def holding(self):
-    return 'holding'
+  def verbpthat(self, v, p):
+    return self.join(self.verbp(v,p), 'that')
   def commanded(self, verb):
     return f"command${verb}"
+  def put_on(self, n1, n2):
+    return f"put_on({n1},{n2})"
   def that(self):
     return 'that'
   def most(self, adj, *args):
     return f"#most({adj},{self.conj(*args)})"
-  def support(self, noun):
-    return self.join('support', noun)
   def it(self):
     return '#it'
   def det(self, name):
     if name == 'that': return name
     return ''
-  def adj(self, name):
-    return name
   def each(self, adj, noun):
     return f"#each({adj},{noun})"
   def prep(self, name):
     if name.endswith('to'): name = name[:-2]
     if name == 'on top of': name = 'above'
     return name
-  def time(self):
-    return ''
-  def told_put(self, n1, p, n2):
-    return f"goal.{p}({n1},{n2})"
-  def cleanoff(self, noun):
-    return self.join('cleanoff', noun)
+  def time(self, name=''):
+    return name
+  def prept(self, name):
+    return name.replace(' ','_')
+  def told(self, c):
+    return self.join('goal', c[:-1])
 
 def main():
-  tests = [
-    'pick up a big red block',
-    'pick up that pyramid',
-    'find a block which is taller than the one you are holding',
-    'put that into the box',
-    'what does the box contain',
-    'what is that pyramid supported by',
-    'how many blocks are not in the box',
-    'is at least one of those narrower than the one which I told you to pick up',
-    'is that supported',
-    'stack up two pyramids',
-    'the blue pyramid is mine',
-    'I own blocks which are not red but I don\'t own anything which supports a pyramid',
-    'do I own the box',
-    'do I own anything in the box',
-    'will you please stack up both of the red blocks and either a green cube or a pyramid',
-    'which cube is sitting on the table',
-    'is there a large block behind a pyramid',
-    'put a small block onto the green cube which supports a pyramid',
-    'put the smallest pyramid on top of the small block',
-    'does the shortest thing the tallest pyramid\'s support supports support anything green',
-    'which green thing does the shortest thing the tallest pyramid\'s support supports support',
-    'what colour is that',
-    'how many things are on top of green cubes',
-    'had you touched any pyramid before I told you to put the green pyramid on the little cube',
-    'how many objects did you touch while you were doing that',
-    'what did the red cube support before you started to clean the red cube off',
-    'which blocks were to the left of the box before you started to clean the red cube off',
-    'put the blue pyramid on the block in the box',
-    'is there anything which is big-er than every pyramid but is not as wide as the thing that supports it',
-    'a "steeple" is a stack which contains two green cubes and a pyramid',
-    'build a steeple',
-    'call the big-est block "superblock"',
-    'have you picked superblock up since we began',
-    'is there anything to the right of the red pyramid',
-  ]
-  for test in tests:
-    print(test)
-    tree = parser.parse(test)
+  for line in open('test.txt','r'):
+    if line[0] == '#': continue
+    line = line.strip().lower()[:-1]
+    print(line)
+    tree = parser.parse(line)
     #print(tree.pretty())
     print(Sem().transform(tree))
     print()
