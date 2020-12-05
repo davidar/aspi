2b996bf8e9c847f0ba128bdd10ea37cc6dd7accd
David A Roberts <d@vidr.cc>
2020-12-05 11:52:16 +1000

    autopep8

--- aspi.py
+++ aspi.py
@@ -8,112 +8,127 @@ import ldcs
 
 ClingoExhausted = sh.ErrorReturnCode_20
 
+
 def replaceall(s, d, d2={}):
-  r = s
-  for k,v in d.items():
-    if k in d2: v += ' ' + d2[k]
-    r = r.replace(k,v)
-  if s == r: return s
-  return replaceall(r, d)
+    r = s
+    for k, v in d.items():
+        if k in d2:
+            v += ' ' + d2[k]
+        r = r.replace(k, v)
+    if s == r:
+        return s
+    return replaceall(r, d)
+
 
 def readfiles(*args):
-  s = ''
-  for name in args:
-    with open(name, 'r') as f:
-      s += f.read()
-  return s
+    s = ''
+    for name in args:
+        with open(name, 'r') as f:
+            s += f.read()
+    return s
+
 
 def clingo(lp):
-  try:
-    result = sh.clingo(_ok_code=[10], _in=lp, outf=2, time_limit=2).stdout
-    values = json.loads(result)['Call'][-1]['Witnesses'][0]['Value']
-    return values
-  except:
-    #print(lp)
-    raise
+    try:
+        result = sh.clingo(_ok_code=[10], _in=lp, outf=2, time_limit=2).stdout
+        values = json.loads(result)['Call'][-1]['Witnesses'][0]['Value']
+        return values
+    except:
+        # print(lp)
+        raise
 
-program = readfiles('world.lp', 'actions.lp', 'planner.lp', 'why.lp', 'prelude.lp')
+
+program = readfiles('world.lp', 'actions.lp',
+                    'planner.lp', 'why.lp', 'prelude.lp')
 facts = set(['moves(0)'])
 counter = 1
 
 with open('macros.lp', 'r') as f:
-  for line in f:
-    ldcs.add_macro(line)
+    for line in f:
+        ldcs.add_macro(line)
 
 while True:
-  cmd = input('>>> ')
-  print(cmd)
+    cmd = input('>>> ')
+    print(cmd)
 
-  if not cmd or cmd.startswith('%'): continue
-  if cmd == 'thanks.':
-    print("YOU'RE WELCOME!")
-    break
+    if not cmd or cmd.startswith('%'):
+        continue
+    if cmd == 'thanks.':
+        print("YOU'RE WELCOME!")
+        break
 
-  declare = cmd.endswith('.')
-  cmd = ldcs.transform(cmd)
-  print('-->', '\n    '.join(cmd.split('\n')))
-  cmd += '\n'
-  if declare:
-    program += cmd
-    print('understood.\n')
-    continue
+    declare = cmd.endswith('.')
+    cmd = ldcs.transform(cmd)
+    print('-->', '\n    '.join(cmd.split('\n')))
+    cmd += '\n'
+    if declare:
+        program += cmd
+        print('understood.\n')
+        continue
 
-  now = int([fact[len('moves('):-1] for fact in facts if fact.startswith('moves(')][0])
-  cmd += '#const now = ' + str(now) + '.\n'
-  cmd += '#const counter = ' + str(counter) + '.\n'
-  cmd += ''.join(fact + '.\n' for fact in facts)
-  try:
-    results = clingo(cmd + program)
-  except ClingoExhausted as e:
-    print('impossible.\n')
-    #print(e.stderr.decode('utf-8'))
-    continue
-  except sh.ErrorReturnCode as e:
-    print(e.stderr.decode('utf-8'), file=sys.stderr)
-    sys.exit(1)
+    now = int([fact[len('moves('):-1]
+               for fact in facts if fact.startswith('moves(')][0])
+    cmd += '#const now = ' + str(now) + '.\n'
+    cmd += '#const counter = ' + str(counter) + '.\n'
+    cmd += ''.join(fact + '.\n' for fact in facts)
+    try:
+        results = clingo(cmd + program)
+    except ClingoExhausted as e:
+        print('impossible.\n')
+        # print(e.stderr.decode('utf-8'))
+        continue
+    except sh.ErrorReturnCode as e:
+        print(e.stderr.decode('utf-8'), file=sys.stderr)
+        sys.exit(1)
 
-  ok = False
-  acts = []
-  shows = []
-  names = {}
-  names_t = {}
-  for result in results:
-    if result.startswith('assert('):
-      result = result[len('assert('):-1]
-      facts.add(result)
-      m = re.fullmatch(r'apply\((.*),\d+\)', result)
-      if m: acts.append(m.group(1).replace(',', ', '))
-    elif result.startswith('retract('):
-      result = result[len('retract('):-1]
-      facts.remove(result)
-    elif result.startswith('what('):
-      result = result[len('what('):-1]
-      m = re.fullmatch(r'apply\((.*),\d+\)', result)
-      if m: result = m.group(1) \
-        .replace('(','[').replace(')',']').replace(',', ', ')
-      m = re.fullmatch(r'history\(\d+,goal\((.*)\)\)', result)
-      if m: result = 'goal.' + m.group(1).replace(',', ', ')
-      shows.append(result)
-    elif result.startswith('history('):
-      facts.add(result)
-    elif result.startswith('already('):
-      result = result[len('already('):-1].replace(',', ', ')
-      print(replaceall(result, names) + '.')
-    elif result.startswith('describe('):
-      result = result[len('describe('):-1].split(',')
-      names[result[0]] = ' '.join(result[1:])
-    elif result.startswith('describe_extra('):
-      result = result[len('describe_extra('):-1].split(',')
-      t = int(result[0])
-      if t not in names_t: names_t[t] = {}
-      names_t[t][result[1]] = ' '.join(result[2:]) \
-        .replace('(','[').replace(')',']').replace('not_','~')
-    elif result == 'ok':
-      ok = True
-  for t,act in enumerate(acts):
-    print(replaceall(act, names, names_t[now+t]) + '!')
-  if ok: print('ok.')
-  shows = [replaceall(show, names, names_t[now]) for show in shows]
-  if shows: print('that(' + ' | '.join(shows) + ').')
-  print()
-  counter += 1
+    ok = False
+    acts = []
+    shows = []
+    names = {}
+    names_t = {}
+    for result in results:
+        if result.startswith('assert('):
+            result = result[len('assert('):-1]
+            facts.add(result)
+            m = re.fullmatch(r'apply\((.*),\d+\)', result)
+            if m:
+                acts.append(m.group(1).replace(',', ', '))
+        elif result.startswith('retract('):
+            result = result[len('retract('):-1]
+            facts.remove(result)
+        elif result.startswith('what('):
+            result = result[len('what('):-1]
+            m = re.fullmatch(r'apply\((.*),\d+\)', result)
+            if m:
+                result = m.group(1) \
+                    .replace('(', '[').replace(')', ']').replace(',', ', ')
+            m = re.fullmatch(r'history\(\d+,goal\((.*)\)\)', result)
+            if m:
+                result = 'goal.' + m.group(1).replace(',', ', ')
+            shows.append(result)
+        elif result.startswith('history('):
+            facts.add(result)
+        elif result.startswith('already('):
+            result = result[len('already('):-1].replace(',', ', ')
+            print(replaceall(result, names) + '.')
+        elif result.startswith('describe('):
+            result = result[len('describe('):-1].split(',')
+            names[result[0]] = ' '.join(result[1:])
+        elif result.startswith('describe_extra('):
+            result = result[len('describe_extra('):-1].split(',')
+            t = int(result[0])
+            if t not in names_t:
+                names_t[t] = {}
+            names_t[t][result[1]] = ' '.join(result[2:]) \
+                .replace('(', '[').replace(')', ']').replace('not_', '~')
+        elif result == 'ok':
+            ok = True
+    for t, act in enumerate(acts):
+        print(replaceall(act, names, names_t[now+t]) + '!')
+    if ok:
+        print('ok.')
+    shows = [replaceall(show, names, names_t[now]) for show in shows]
+    if shows:
+        print('that(' + ' | '.join(shows) + ').')
+    print()
+    counter += 1
--- ldcs.py
+++ ldcs.py
@@ -50,107 +50,141 @@ multijoin: func "[" disjs [";" disjs] "]"
 
 parser = lark.Lark(ebnf)
 
+
 @lark.v_args(inline=True)
 class LDCS(lark.Transformer):
-  def __init__(self):
-    self.count = 0
-    self.counts = {}
-    self.body = ''
-    self.rules = []
-  def gensym(self):
-    sym = string.ascii_uppercase[self.count]
-    self.count += 1
-    return sym
-  def genpred(self, prefix):
-    if prefix not in self.counts: self.counts[prefix] = 0
-    self.counts[prefix] += 1
-    return lambda x: prefix + str(self.counts[prefix]) + '(' + x + ')'
-  def lift(self, lam, prefix='lifted'):
-    f = self.genpred(prefix)
-    z = self.gensym()
-    self.rules.append(f(z) + ' :- ' + lam(z) + '.')
-    return f
-  def expand_macro(self, name, *args):
-    params, tree = macros[name]
-    subst = dict(zip(params,args))
-    return RuleBody(subst, self.gensym).transform(tree)
+    def __init__(self):
+        self.count = 0
+        self.counts = {}
+        self.body = ''
+        self.rules = []
 
-  def command(self, value):
-    if len(value) == 1: value = 'what(' + value + ')'
-    if self.body: value += ' :- ' + self.body
-    value += '.'
-    self.rules.insert(0, value)
-    return '\n'.join(self.rules).replace(';,', ';')
-  def goal(self, value):
-    body = self.body
-    if ';' in self.body:
-      f = self.genpred('goal')
-      self.rules.append(f(value) + ' :- ' + body + '.')
-      value = self.gensym()
-      body = f(value)
-    self.rules.insert(0, '{ goal(' + value + ') : ' + body + ' } = 1.')
-    return '\n'.join(self.rules).replace(';,', ';')
-  def pred(self, name, *args):
-    if not args: return name
-    return name + '(' + ','.join(args) + ')'
-  def atom(self, name):
-    return name
-  def ldcs(self, lam):
-    x = self.gensym()
-    if self.body: self.body += ', '
-    self.body += lam(x)
-    return x
-  def disjs(self, *args):
-    return list(args)
-  def disj(self, *lams):
-    if len(lams) == 1: return lams[0]
-    f = self.genpred('disjunction')
-    x = self.gensym()
-    self.rules.extend(f(x) + ' :- ' + lam(x) + '.' for lam in lams)
-    return f
-  def conj(self, *lams):
-    return lambda x: ', '.join(lam(x) for lam in lams)
-  def constant(self, c):
-    if c in string.ascii_uppercase: c = 'Mu' + c
-    return lambda x: x + ' = ' + c
-  def func(self, name):
-    if name in macros: return lambda *args: self.expand_macro(name,*args)
-    return lambda *args: name + '(' + ','.join(args) + ')'
-  def compose(self, name, lam):
-    return lambda *args: name + '(' + lam(*args) + ')'
-  def flip(self, lam):
-    return lambda *args: lam(*reversed(args))
-  def join(self, rel, lam):
-    y = self.gensym()
-    return lambda x: rel(x,y) + ', ' + lam(y)
-  def neg(self, lam):
-    if ' ' in lam('_'): lam = self.lift(lam)
-    return lambda x: 'not ' + lam(x)
-  def aggregation(self, op, lam):
-    y = self.gensym()
-    if ';' in lam(y): lam = self.lift(lam, 'aggregation')
-    if op == 'count':
-      return lambda x: x + ' = #count { ' + y + ' : ' + lam(y) + ' }'
-    elif op == 'any':
-      f = self.genpred('any')
-      self.rules.append(f('true')  + ' :- ' + lam(y) + '.')
-      self.rules.append(f('false') + ' :- not ' + f('true') + '.')
-      return f
-  def superlative(self, op, rel, lam):
-    y = self.gensym()
-    if ';' in lam(y): lam = self.lift(lam, 'superlative')
-    if op == 'most':
-      return lambda x: rel(x,y) + ' : ' + lam(y) + ', ' + x + ' != ' + y + '; ' + lam(x)
-    elif op == 'each':
-      return lambda x: rel(x,y) + ' : ' + lam(y) + ';'
-  def unify(self, pred):
-    return lambda x: x + ' = ' + pred
-  def multijoin(self, rel, lams_tail, lams_head = []):
-    lams_head.reverse()
-    xs = [self.gensym() for lam in lams_head]
-    zs = [self.gensym() for lam in lams_tail]
-    lams = zip(lams_head + lams_tail, xs + zs)
-    return lambda y: rel(*xs,y,*zs) + ', ' + ', '.join(lam(x) for lam,x in lams)
+    def gensym(self):
+        sym = string.ascii_uppercase[self.count]
+        self.count += 1
+        return sym
+
+    def genpred(self, prefix):
+        if prefix not in self.counts:
+            self.counts[prefix] = 0
+        self.counts[prefix] += 1
+        return lambda x: prefix + str(self.counts[prefix]) + '(' + x + ')'
+
+    def lift(self, lam, prefix='lifted'):
+        f = self.genpred(prefix)
+        z = self.gensym()
+        self.rules.append(f(z) + ' :- ' + lam(z) + '.')
+        return f
+
+    def expand_macro(self, name, *args):
+        params, tree = macros[name]
+        subst = dict(zip(params, args))
+        return RuleBody(subst, self.gensym).transform(tree)
+
+    def command(self, value):
+        if len(value) == 1:
+            value = 'what(' + value + ')'
+        if self.body:
+            value += ' :- ' + self.body
+        value += '.'
+        self.rules.insert(0, value)
+        return '\n'.join(self.rules).replace(';,', ';')
+
+    def goal(self, value):
+        body = self.body
+        if ';' in self.body:
+            f = self.genpred('goal')
+            self.rules.append(f(value) + ' :- ' + body + '.')
+            value = self.gensym()
+            body = f(value)
+        self.rules.insert(0, '{ goal(' + value + ') : ' + body + ' } = 1.')
+        return '\n'.join(self.rules).replace(';,', ';')
+
+    def pred(self, name, *args):
+        if not args:
+            return name
+        return name + '(' + ','.join(args) + ')'
+
+    def atom(self, name):
+        return name
+
+    def ldcs(self, lam):
+        x = self.gensym()
+        if self.body:
+            self.body += ', '
+        self.body += lam(x)
+        return x
+
+    def disjs(self, *args):
+        return list(args)
+
+    def disj(self, *lams):
+        if len(lams) == 1:
+            return lams[0]
+        f = self.genpred('disjunction')
+        x = self.gensym()
+        self.rules.extend(f(x) + ' :- ' + lam(x) + '.' for lam in lams)
+        return f
+
+    def conj(self, *lams):
+        return lambda x: ', '.join(lam(x) for lam in lams)
+
+    def constant(self, c):
+        if c in string.ascii_uppercase:
+            c = 'Mu' + c
+        return lambda x: x + ' = ' + c
+
+    def func(self, name):
+        if name in macros:
+            return lambda *args: self.expand_macro(name, *args)
+        return lambda *args: name + '(' + ','.join(args) + ')'
+
+    def compose(self, name, lam):
+        return lambda *args: name + '(' + lam(*args) + ')'
+
+    def flip(self, lam):
+        return lambda *args: lam(*reversed(args))
+
+    def join(self, rel, lam):
+        y = self.gensym()
+        return lambda x: rel(x, y) + ', ' + lam(y)
+
+    def neg(self, lam):
+        if ' ' in lam('_'):
+            lam = self.lift(lam)
+        return lambda x: 'not ' + lam(x)
+
+    def aggregation(self, op, lam):
+        y = self.gensym()
+        if ';' in lam(y):
+            lam = self.lift(lam, 'aggregation')
+        if op == 'count':
+            return lambda x: x + ' = #count { ' + y + ' : ' + lam(y) + ' }'
+        elif op == 'any':
+            f = self.genpred('any')
+            self.rules.append(f('true') + ' :- ' + lam(y) + '.')
+            self.rules.append(f('false') + ' :- not ' + f('true') + '.')
+            return f
+
+    def superlative(self, op, rel, lam):
+        y = self.gensym()
+        if ';' in lam(y):
+            lam = self.lift(lam, 'superlative')
+        if op == 'most':
+            return lambda x: rel(x, y) + ' : ' + lam(y) + ', ' + x + ' != ' + y + '; ' + lam(x)
+        elif op == 'each':
+            return lambda x: rel(x, y) + ' : ' + lam(y) + ';'
+
+    def unify(self, pred):
+        return lambda x: x + ' = ' + pred
+
+    def multijoin(self, rel, lams_tail, lams_head=[]):
+        lams_head.reverse()
+        xs = [self.gensym() for lam in lams_head]
+        zs = [self.gensym() for lam in lams_tail]
+        lams = zip(lams_head + lams_tail, xs + zs)
+        return lambda y: rel(*xs, y, *zs) + ', ' + ', '.join(lam(x) for lam, x in lams)
+
 
 rule_ebnf = r'''
 %import common.DIGIT
@@ -178,43 +212,59 @@ head: ATOM "(" var ("," var)* ")"
 
 rule_parser = lark.Lark(rule_ebnf)
 
+
 @lark.v_args(inline=True)
 class RuleHead(lark.Transformer):
-  def rule(self, head, *body):
-    return head
-  def var(self, name):
-    return name
-  def head(self, name, *args):
-    return str(name), [str(arg) for arg in args]
+    def rule(self, head, *body):
+        return head
+
+    def var(self, name):
+        return name
+
+    def head(self, name, *args):
+        return str(name), [str(arg) for arg in args]
+
 
 @lark.v_args(inline=True)
 class RuleBody(lark.Transformer):
-  def __init__(self, subst, gensym):
-    self.subst = subst
-    self.gensym = gensym
-  def rule(self, head, *body):
-    return ', '.join(body)
-  def pred(self, name, *args):
-    if not args: name
-    return name + '(' + ','.join(args) + ')'
-  def predop(self, *args):
-    return ' '.join(args)
-  def var(self, name):
-    if name not in self.subst:
-      self.subst[name] = self.gensym()
-    return self.subst[name]
+    def __init__(self, subst, gensym):
+        self.subst = subst
+        self.gensym = gensym
+
+    def rule(self, head, *body):
+        return ', '.join(body)
+
+    def pred(self, name, *args):
+        if not args:
+            name
+        return name + '(' + ','.join(args) + ')'
+
+    def predop(self, *args):
+        return ' '.join(args)
+
+    def var(self, name):
+        if name not in self.subst:
+            self.subst[name] = self.gensym()
+        return self.subst[name]
+
 
 def transform(s):
-  tree = parser.parse(s)
-  return LDCS().transform(tree)
+    tree = parser.parse(s)
+    return LDCS().transform(tree)
+
 
 macros = {}
 
+
 def add_macro(s):
-  tree = rule_parser.parse(s)
-  name, args = RuleHead().transform(tree)
-  macros[name] = args, tree
+    tree = rule_parser.parse(s)
+    name, args = RuleHead().transform(tree)
+    macros[name] = args, tree
+
 
 def main():
-  print(transform(input()))
-if __name__ == '__main__': main()
+    print(transform(input()))
+
+
+if __name__ == '__main__':
+    main()
