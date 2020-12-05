c6d4f5e30f91e1a76b741961273118ab42a1520a
David A Roberts <d@vidr.cc>
2020-12-05 21:06:03 +1000

    hanoi

--- test_aspi.py
+++ test_aspi.py
@@ -5,6 +5,14 @@ def test_euler(script_runner):
     assert ret.stderr == ''
 
 
+def test_hanoi(script_runner):
+    ret = script_runner.run('./aspi.py', 'hanoi/world.lp', 'hanoi/actions.lp',
+                            stdin=open('hanoi/test.in', 'r'))
+    assert ret.success
+    assert ret.stdout == open('hanoi/test.out', 'r').read()
+    assert ret.stderr == ''
+
+
 def test_shrdlu(script_runner):
     ret = script_runner.run('./aspi.py', 'shrdlu/world.lp', 'shrdlu/actions.lp',
                             stdin=open('shrdlu/test.in', 'r'))
