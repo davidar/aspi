a821ba440c88dd77aaa014340140cb2ed225a1f9
David A Roberts <d@vidr.cc>
2020-12-13 17:30:02 +1000

    golf

--- test_aspi.py
+++ test_aspi.py
@@ -1,21 +1,22 @@
-def test_euler(script_runner):
-    ret = script_runner.run('./aspi.py', stdin=open('euler/test.in', 'r'))
+def run(s, name, plan=False):
+    args = [name + '/world.lp', name + '/actions.lp'] if plan else []
+    ret = s.run('./aspi.py', *args, stdin=open(name + '/test.in', 'r'))
     assert ret.success
-    assert ret.stdout == open('euler/test.out', 'r').read()
+    assert ret.stdout == open(name + '/test.out', 'r').read()
     assert ret.stderr == ''
+
+
+def test_euler(script_runner):
+    run(script_runner, 'euler')
+
+
+def test_golf(script_runner):
+    run(script_runner, 'golf')
 
 
 def test_hanoi(script_runner):
-    ret = script_runner.run('./aspi.py', 'hanoi/world.lp', 'hanoi/actions.lp',
-                            stdin=open('hanoi/test.in', 'r'))
-    assert ret.success
-    assert ret.stdout == open('hanoi/test.out', 'r').read()
-    assert ret.stderr == ''
+    run(script_runner, 'hanoi', True)
 
 
 def test_shrdlu(script_runner):
-    ret = script_runner.run('./aspi.py', 'shrdlu/world.lp', 'shrdlu/actions.lp',
-                            stdin=open('shrdlu/test.in', 'r'))
-    assert ret.success
-    assert ret.stdout == open('shrdlu/test.out', 'r').read()
-    assert ret.stderr == ''
+    run(script_runner, 'shrdlu', True)
