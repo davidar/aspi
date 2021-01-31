fcc2ef668f123008818ae0b9c748dd8a63b5ac53
David A Roberts <d@vidr.cc>
2021-01-31 01:53:47 +0000

    parametrise tests

--- test_aspi.py
+++ test_aspi.py
@@ -1,28 +1,14 @@
-def run(s, name):
-    ret = s.run('./aspi.py', stdin=open(f'test/{name}.ldcs', 'r'))
+import pytest
+
+scripts = ['euler', 'golf', 'hanoi', 'shortest-path', 'zebra']
+
+
+@pytest.mark.parametrize('name', scripts)
+def test_script(script_runner, name):
+    ret = script_runner.run('./aspi.py', stdin=open(f'test/{name}.ldcs', 'r'))
     assert ret.success
     assert ret.stdout == open(f'test/{name}.log', 'r').read()
     assert ret.stderr == ''
-
-
-def test_euler(script_runner):
-    run(script_runner, 'euler')
-
-
-def test_golf(script_runner):
-    run(script_runner, 'golf')
-
-
-def test_hanoi(script_runner):
-    run(script_runner, 'hanoi')
-
-
-def test_shortest_path(script_runner):
-    run(script_runner, 'shortest-path')
-
-
-def test_zebra(script_runner):
-    run(script_runner, 'zebra')
 
 
 def test_shrdlu(script_runner):
